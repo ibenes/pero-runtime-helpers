5e224e0cb1fde6fdf97ae4dc9ad96ff600809325
Benes Karel <ibenes@fit.vutbr.cz>
2022-08-18 15:33:54 +0200

    BUGFIX: Add missing backslash

--- gpu_check.sh
+++ gpu_check.sh
@@ -3,7 +3,7 @@ then
     :  # a NOP to avoid syntax error
 else
     send-mail.py \
-        -f 'pero@fit.vutbr.cz'
+        -f 'pero@fit.vutbr.cz' \
         -t 'ibenes@fit.vutbr.cz' \
         -s 'GPU check' \
         -c "GPU check on $(hostname) failed" \
