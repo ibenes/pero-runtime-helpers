95f623d8a7fdfe5dc347c3fdd1c8cac32c1c6254
Benes Karel <ibenes@fit.vutbr.cz>
2022-08-18 14:50:13 +0200

    Get rid of an absolute path

--- gpu_check.sh
+++ gpu_check.sh
@@ -4,7 +4,7 @@ if [ nvidia-smi ]
 then
 	:  # a NOP to avoid syntax error
 else
-	python3 /home/pero/restarting/send-mail.py \
+	send-mail.py \
 		-t 'ibenes@fit.vutbr.cz' \
 		-s 'GPU check' \
 		-c 'GPU check failed' \
