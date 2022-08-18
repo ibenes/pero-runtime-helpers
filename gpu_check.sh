7698f41b25da860607b8512d74203057476e9859
Benes Karel <ibenes@fit.vutbr.cz>
2022-08-18 15:29:54 +0200

    Clean tabs; fix sender address to be universal; include hostname

--- gpu_check.sh
+++ gpu_check.sh
@@ -1,12 +1,11 @@
-echo 'Running gpu check at ' $(date) >> ~/cron-test.txt
-
 if [ nvidia-smi ] 
 then
-	:  # a NOP to avoid syntax error
+    :  # a NOP to avoid syntax error
 else
-	send-mail.py \
-		-t 'ibenes@fit.vutbr.cz' \
-		-s 'GPU check' \
-		-c 'GPU check failed' \
-		>> ~/send-mail.txt
+    send-mail.py \
+        -f 'pero@fit.vutbr.cz'
+        -t 'ibenes@fit.vutbr.cz' \
+        -s 'GPU check' \
+        -c "GPU check on $(hostname) failed" \
+        >> ~/send-mail.txt
 fi
